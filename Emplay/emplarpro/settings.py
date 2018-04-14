96479a5be3a08a9ec34bda598a2567a9f7c8827c
Python Developer <dev@brio.co.in>
2018-04-14 23:05:05 +0530

    dksjgkj

--- Emplay/emplarpro/settings.py
+++ Emplay/emplarpro/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'emplarpro.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
- 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -85,8 +85,9 @@ DATABASES = {
 
     }
 }
-# import dj_database_url
-# DATABASES = { 'default': dj_database_url.config( default=dj_database_url.config('DATABASE_URL') ) }
+'''
+import dj_database_url
+DATABASES = { 'default': dj_database_url.config( default=dj_database_url.config('DATABASE_URL') ) }
 
 
 
