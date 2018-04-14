fd3e74be947f5aea810946f264288556f0ef7fb2
Python Developer <dev@brio.co.in>
2018-04-14 23:10:14 +0530

    ddd

--- Emplay/emplarpro/settings.py
+++ Emplay/emplarpro/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 #         os.path.join(BASE_DIR, 'static'),
 # )
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
 
